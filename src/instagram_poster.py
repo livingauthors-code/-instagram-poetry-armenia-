4c674bcc4cfd0fcfac80f60f2693f6f31a7a8acd
livingauthors-code <livingauthors@gmail.com>
2025-11-11 23:08:54 +0400

    Update instagram_poster.py

--- src/instagram_poster.py
+++ src/instagram_poster.py
@@ -1,4 +1,13 @@
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import pandas as pd
+import random
+from config import Config
+
+
+import os
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
